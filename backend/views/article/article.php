f79569ba6aef10b529ea915bdabe3b267ebec1f3
liu <3046587188@qq.com>
2016-10-17 16:08:40 +0800

    breadcrumbs

--- backend/views/article/article.php
+++ backend/views/article/article.php
@@ -1,2 +1,24 @@
+<?php
+use yii\helpers\Url;
+use yii\widgets\Breadcrumbs;
+echo Breadcrumbs::widget([
+    'homeLink'=>['label'=>'文章首页','url'=>Url::toRoute('article/index')],
+    'links'=>[
+        [
+            'label'=>$article['cate_id'],
+            'url'=>'',
+            'template' => "<li><b>{link}</b></li>\n", // template for this link only
+        ],
+        [
+            'label' => $article['art_title'] ,
+            'url'=>'',
+        ],
+
+      ],
+    'options'=>[
+        'class'=>'breadcrumb',
+    ]
+]);
+?>
 <h3><?=$article['art_title']?></h3>
 <p><?=$article['art_content']?></p>
