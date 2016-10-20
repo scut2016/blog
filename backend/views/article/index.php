74173c2858a9f0f367103ff4a8b8cf179a8d60e6
liu <3046587188@qq.com>
2016-10-20 14:22:24 +0800

    数据映射

--- backend/views/article/index.php
+++ backend/views/article/index.php
@@ -32,6 +32,7 @@ else
        <td><a href="<?=\yii\helpers\Url::toRoute(['article/article','id'=>$v['art_id']])?>"><?=$v['art_title']?></a></td>
 <!--        <td class="active"><a href="index.php?r=article/article&id=--><?//=$v['art_id']?><!--">--><?//=$v['art_title']?><!--</a></td>-->
         <td class="active"><a href="<?=\yii\helpers\Url::toRoute(['category/category','id'=>$v['category']['cate_id']])?>"><?=$v['category']['cate_name']?></a></td>
+        <td><?=$v['art_view']?></td>
     </tr>
     <? endforeach;?>
 
